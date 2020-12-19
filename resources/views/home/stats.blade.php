8e26c0ed59a90c1b5f2d60cdece35e559268e48e
Dan Harrin <git@danharrin.com>
2020-12-19 08:54:58 +0000

    wip

--- resources/views/home/stats.blade.php
+++ resources/views/home/stats.blade.php
@@ -1,4 +1,19 @@
-<div class="max-w-4xl mx-auto space-y-8">
+<div x-init="interval = setInterval(() => {
+    const rect = $el.getBoundingClientRect()
+    if ((rect.bottom < 0) || (rect.top >= window.innerHeight)) return
+    clearInterval(interval)
+    setInterval(() => { if (currentCommunityMembers < maxCommunityMembers) { currentCommunityMembers += (maxCommunityMembers / 30) } else { currentCommunityMembers = maxCommunityMembers } }, 50)
+    setInterval(() => { if (currentPartners < maxPartners) { currentPartners += (maxPartners / 30) } else { currentPartners = maxPartners } }, 50)
+    setInterval(() => { if (currentQuestionsAnswered < maxQuestionsAnswered) { currentQuestionsAnswered += (maxQuestionsAnswered / 30) } else { currentQuestionsAnswered = maxQuestionsAnswered } }, 50)
+}, 50)" x-data="{
+    currentCommunityMembers: 0,
+    currentPartners: 0,
+    currentQuestionsAnswered: 0,
+    interval: null,
+    maxCommunityMembers: 66423,
+    maxPartners: {{ $stats->partners }},
+    maxQuestionsAnswered: {{ $stats->answeredQuestions }},
+}" class="max-w-4xl mx-auto space-y-8">
     <div class="max-w-4xl mx-auto text-center px-4 sm:px-6 lg:px-8">
         <h2 class="text-3xl leading-9 font-display sm:text-4xl sm:leading-10">
             Trusted by the largest political communities
@@ -16,9 +31,7 @@
             <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-300">
                 questions answered
             </dt>
-            <dd x-data="{ current: 0, max: {{ $stats->answeredQuestions }} }"
-                x-init="setInterval(() => { if (current < max) { current += (max / 20) } else { current = max } }, 25)"
-                x-text="Math.round(current).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '+'"
+            <dd x-text="Math.round(currentQuestionsAnswered).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '+'"
                 class="order-1 text-5xl leading-none font-display text-indigo-500"
             >0
             </dd>
@@ -30,9 +43,7 @@
             <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-300">
                 communities
             </dt>
-            <dd x-data="{ current: 0, max: {{ $stats->partners }} }"
-                x-init="setInterval(() => { if (current < max) { current += (max / 20) } else { current = max } }, 25)"
-                x-text="Math.round(current).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+            <dd x-text="Math.round(currentPartners).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
                 class="order-1 text-5xl leading-none font-display text-indigo-500"
             >0
             </dd>
@@ -42,9 +53,7 @@
             <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-300">
                 community members
             </dt>
-            <dd x-data="{ current: 0, max: 66423 }"
-                x-init="setInterval(() => { if (current < max) { current += (max / 20) } else { current = max } }, 25)"
-                x-text="Math.round(current).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '+'"
+            <dd x-text="Math.round(currentCommunityMembers).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '+'"
                 class="order-1 text-5xl leading-none font-display text-indigo-500"
             >0
             </dd>
